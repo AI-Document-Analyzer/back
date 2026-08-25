cdbce215117f8b1bce96af5f06cc0ba8066808bf
임건표 <imgeonpyo@imgeonpyoui-MacBookAir.local>
2026-08-25 17:42:41 +0900

    user and document models

---
+++ app/models/user.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, DateTime, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+
+from app.db.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String(255), unique=True, nullable=False, index=True)
+    name = Column(String(100), nullalbe=False)
+    password_hash = Column(String(255), nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    documents = relationship("Document", back_populates="user")
